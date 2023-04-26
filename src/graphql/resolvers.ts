0e522d545a4cf0c5561823a1f7dc94581ba60531
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-04-25 23:56:53 -0400

    Comment out line

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -2,9 +2,9 @@ import { Context } from "./context";
 
 export const resolvers = {
   Query: {
-    hello: () => "world",
-    posts: async (parent: any, args: any, context: Context) => {
-      context.prisma.post.findMany();
-    }
+    hello: () => "world"
+    // posts: async (parent: any, args: any, context: Context) => {
+    //   context.prisma.post.findMany();
+    // }
   }
 };
