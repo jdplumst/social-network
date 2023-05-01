af8ff45fb20080e6e6993d7a5cd84a8c7a9ef142
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-05-01 16:36:28 -0400

    Add signup page

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -4,17 +4,17 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Social Network - Login</title>
+        <title>Social Network - Signup</title>
         <meta name="description" content="Social Network" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <div className="bg-slate-300 min-h-screen flex justify-center items-center pt-10 text-black">
+        <div className="bg-slate-300 min-h-screen flex justify-center items-center pt-10">
           <form
-            // onSubmit={handleLogin}
+            // onSubmit={handleSignup}
             className="flex flex-col p-10 bg-white w-1/2 h-1/3">
-            <h3 className="text-3xl text-center pb-5 font-bold">Log In</h3>
+            <h3 className="text-3xl text-center pb-5 font-bold">Sign Up</h3>
             <label className="font-bold text-xl">Email:</label>
             <input
               type="email"
@@ -29,11 +29,11 @@ export default function Home() {
               // value={password}
               className="p-2 mb-5 border-solid border-2 border-slate-200 focus:border-slate-500 focus:outline-none rounded-lg block w-full"
             />
-            <button className="text-xl bg-purple-500 hover:bg-purple-700 hover:cursor-pointer text-white p-4 rounded-lg font-bold w-1/4 mx-auto">
-              Log In
+            <button className="text-xl bg-green-500 hover:bg-green-700 hover:cursor-pointer text-white p-4 rounded-lg font-bold w-1/4 mx-auto">
+              Sign Up
             </button>
             {/* {error && (
-              <div className="bg-pink-200 border-solid border-4 border-pink-300 mt-5 p-2 w-4/5">
+              <div className="bg-pink-200 border-solid border-4 border-pink-300 mx-auto mt-5 p-2 w-3/5 text-center">
                 {error}
               </div>
             )} */}
