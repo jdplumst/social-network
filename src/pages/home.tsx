46c194f6240a827463558acd241288fae255124d
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-05-16 00:02:05 -0400

    Add textarea for users to create posts

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,27 @@
-import { useQuery, gql } from "@apollo/client";
+import { useQuery, gql, useMutation } from "@apollo/client";
 import Head from "next/head";
 import { useRouter } from "next/router";
-import { useState } from "react";
+import React, { useState } from "react";
+
+interface Profile {
+  id: string;
+  firstName: string;
+  lastName: string;
+  location: string;
+  occupation: string;
+  gender: string;
+  birthday: Date;
+  profilePicture: string;
+  profileCompleted: boolean;
+}
+
+interface Post {
+  id: String;
+  profileId: String;
+  description: String;
+  createDate: Date;
+  modifyDate: Date;
+}
 
 const GET_USER_PROFILE = gql`
   query getUserProfile {
@@ -45,9 +65,23 @@ const GET_POSTS = gql`
   }
 `;
 
+const CREATE_POST = gql`
+  mutation CreatePost($profileId: String, $description: String) {
+    createPost(profileId: $profileId, description: $description) {
+      id
+      description
+      profileId
+      createDate
+      modifyDate
+    }
+  }
+`;
+
 export default function Home() {
   const [load, setLoad] = useState(true);
   const { push } = useRouter();
+
+  const [profile, setProfile] = useState<Profile>();
   useQuery(GET_USER_PROFILE, {
     onCompleted(data) {
       if (!data.userProfile) {
@@ -55,6 +89,7 @@ export default function Home() {
       } else if (!data.userProfile.profileCompleted) {
         push("/onboarding/picture");
       } else if (data.userProfile.profileCompleted) {
+        setProfile(data.userProfile);
         setLoad(false);
       }
     },
@@ -65,13 +100,35 @@ export default function Home() {
 
   // const { loading, data } = useQuery(GET_PROFILES);
 
-  const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [postLength, setPostLength] = useState(0);
+  const [description, setDescription] = useState("");
+
+  const [createPost, { data, loading: loadingPost, error: postError }] =
+    useMutation(CREATE_POST);
 
   const { loading } = useQuery(GET_POSTS, {
     onCompleted(data) {
       setPosts(data.posts);
     }
   });
+
+  const handlePostInput = (e: React.FormEvent<HTMLTextAreaElement>) => {
+    setDescription((e.target as HTMLTextAreaElement).value);
+    setPostLength((e.target as HTMLTextAreaElement).value.length);
+  };
+
+  const handleCreatePost = () => {
+    createPost({
+      variables: { profileId: profile?.id, description: description },
+      onCompleted(data, clientOptions) {
+        setPosts((prevPosts) => [data.createPost, ...prevPosts]);
+      },
+      onError(error, clientOptions) {
+        console.log(error.message);
+      }
+    });
+  };
 
   if (loading || load) return <p>Loading</p>;
 
@@ -84,6 +141,24 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="color min-h-screen">
+        <div className="mx-auto mb-5 w-1/2 rounded-lg border-2 border-slate-300 p-2">
+          <div className="flex justify-center">
+            <textarea
+              placeholder="Write a Post!"
+              maxLength={255}
+              onInput={(e) => handlePostInput(e)}
+              className="mb-5 w-full border-2 border-black bg-inherit p-2 text-inherit"></textarea>
+          </div>
+          <div className="flex justify-between">
+            <span className="text-[12px]">{postLength}/255</span>
+            <button
+              onClick={() => handleCreatePost()}
+              className="ml-5 rounded-lg bg-purple-500 px-4 py-2 font-bold text-white hover:cursor-pointer hover:bg-purple-700">
+              Add Post
+            </button>
+          </div>
+          {postError && <div className="text-red-500">{postError.message}</div>}
+        </div>
         <div className="grid justify-center gap-5 p-4">
           {posts.map((p: any) => (
             <div key={p.id} className="border-color border-2 p-4">
