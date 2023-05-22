d33256cd9c92314b01f8be75fb25cfaa847c099c
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-05-21 23:10:31 -0400

    Use dayjs to display dates on posts

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -11,6 +11,10 @@ import { gql } from "@/client-gen";
 import { Post, Profile } from "@/client-gen/graphql";
 import LoadingSpinner from "@/components/LoadingSpinner";
 import LoadingPage from "@/components/LoadingPage";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+
+dayjs.extend(relativeTime);
 
 const GET_USER_PROFILE = gql(`
   query getUserProfile {
@@ -188,73 +192,80 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="color min-h-screen p-4">
-        <div className="border-color mx-auto mb-5 w-1/2 rounded-lg border-2 p-2">
-          <div className="flex justify-center">
-            <textarea
-              placeholder="Write a Post!"
-              maxLength={255}
-              onInput={(e) => handlePostInput(e)}
-              value={description}
-              className="mb-5 w-full border-2 border-black bg-inherit p-2 text-inherit"></textarea>
+      <main className="color flex min-h-screen justify-center p-4">
+        <div className="w-1/2">
+          <div className="border-color mx-auto mb-5 w-full rounded-lg border-2 p-2">
+            <div className="flex justify-center">
+              <textarea
+                placeholder="Write a Post!"
+                maxLength={255}
+                onInput={(e) => handlePostInput(e)}
+                value={description}
+                className="mb-5 w-full border-2 border-black bg-inherit p-2 text-inherit"></textarea>
+            </div>
+            <div className="flex justify-between">
+              <span className="text-[12px]">{postLength}/255</span>
+              <button
+                onClick={() => handleCreatePost()}
+                disabled={disabled}
+                className="ml-5 rounded-lg bg-purple-600 px-4 py-2 font-bold text-white hover:cursor-pointer hover:bg-purple-700">
+                {loadingPost ? <LoadingSpinner /> : "Add Post"}
+              </button>
+            </div>
+            {postError && (
+              <div className="text-red-500">{postError.message}</div>
+            )}
           </div>
-          <div className="flex justify-between">
-            <span className="text-[12px]">{postLength}/255</span>
+          <div className="flex justify-center gap-5 pb-4">
             <button
-              onClick={() => handleCreatePost()}
-              disabled={disabled}
-              className="ml-5 rounded-lg bg-purple-600 px-4 py-2 font-bold text-white hover:cursor-pointer hover:bg-purple-700">
-              {loadingPost ? <LoadingSpinner /> : "Add Post"}
+              onClick={() => refetchPosts()}
+              className="rounded-lg bg-purple-600 p-4 text-white hover:cursor-pointer hover:bg-purple-700">
+              All Posts
+            </button>
+            <button
+              onClick={() => refetchFollowingPosts()}
+              className="rounded-lg bg-purple-600 p-4 text-white hover:cursor-pointer hover:bg-purple-700">
+              Posts of those you follow
             </button>
           </div>
-          {postError && <div className="text-red-500">{postError.message}</div>}
-        </div>
-        <div className="flex justify-center gap-5 pb-4">
-          <button
-            onClick={() => refetchPosts()}
-            className="rounded-lg bg-purple-600 p-4 text-white hover:cursor-pointer hover:bg-purple-700">
-            All Posts
-          </button>
-          <button
-            onClick={() => refetchFollowingPosts()}
-            className="rounded-lg bg-purple-600 p-4 text-white hover:cursor-pointer hover:bg-purple-700">
-            Posts of those you follow
-          </button>
-        </div>
-        <div className="flex justify-center">
-          <div className="grid w-screen grid-cols-1 justify-center gap-5">
-            {loadingPosts ||
-            loadingFollowingPosts ||
-            networkPosts === NetworkStatus.refetch ||
-            networkFollowingPosts === NetworkStatus.refetch ? (
-              <LoadingSpinner />
-            ) : (
-              posts!.map((p) => (
-                <div
-                  key={p.id}
-                  className="border-color mx-auto w-1/3 overflow-auto border-2 p-4">
-                  <div className="flex items-center gap-4 pb-2">
-                    <img
-                      src={p.profile!.profilePicture!}
-                      alt={
-                        p.profile!.firstName +
-                        ` ` +
-                        p.profile!.lastName +
-                        `\'s profile picture`
-                      }
-                      className="inline h-14 w-14"
-                    />{" "}
-                    <div className="flex items-end gap-4">
-                      <span className="text-3xl font-bold">
-                        {p.profile!.firstName} {p.profile!.lastName}
-                      </span>
-                      <span>({p.createDate})</span>
+          <div className="flex justify-center">
+            <div className="grid w-screen grid-cols-1 justify-center gap-5">
+              {loadingPosts ||
+              loadingFollowingPosts ||
+              networkPosts === NetworkStatus.refetch ||
+              networkFollowingPosts === NetworkStatus.refetch ? (
+                <LoadingSpinner />
+              ) : (
+                posts!.map((p) => (
+                  <div
+                    key={p.id}
+                    className="border-color mx-auto w-full overflow-auto border-2 p-4">
+                    <div className="flex items-center gap-4 pb-2">
+                      <img
+                        src={p.profile!.profilePicture!}
+                        alt={
+                          p.profile!.firstName +
+                          ` ` +
+                          p.profile!.lastName +
+                          `\'s profile picture`
+                        }
+                        className="inline h-14 w-14"
+                      />{" "}
+                      <div className="flex gap-2">
+                        <span className="text-2xl font-bold">
+                          {p.profile!.firstName} {p.profile!.lastName}
+                        </span>
+                        <span className="text-2xl">·</span>
+                        <span className="text-2xl">
+                          {dayjs(p.createDate).fromNow()}
+                        </span>
+                      </div>
                     </div>
+                    <span className="text-xl">{p.description}</span>
                   </div>
-                  <span className="text-xl">{p.description}</span>
-                </div>
-              ))
-            )}
+                ))
+              )}
+            </div>
           </div>
         </div>
       </main>
